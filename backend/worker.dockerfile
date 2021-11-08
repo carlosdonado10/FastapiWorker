f02fd40ffc1b8746b56b60b211c7909b15b2adb0
Donado <Donado.Carlos@bcg.com>
2021-11-08 16:18:36 +0100

    update dockerfiles

--- backend/worker.dockerfile
+++ backend/worker.dockerfile
@@ -3,4 +3,4 @@ WORKDIR code/workers
 COPY ./requirements.txt /code/workers/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/workers/requirements.txt
 COPY . /code/workers
-CMD ["celery", "worker", "--loglevel=INFO"]
+CMD ["celery", "-A", "worker", "worker", "--loglevel=INFO"]
--- backend/worker.py
+++ backend/worker.py
@@ -1,10 +1,14 @@
 from celery import Celery
 from time import sleep
+# from backend.base import settings
 
-app = Celery('workers', broker='pyamqp://guest@rabbitmq//')
+app = Celery('workers', broker_url='amqp://guest@rabbitmq:5672/rabbitmq')
 
 @app.task
 def execute_simulation(something: str) -> str:
     sleep(30)
     print(something)
     return something
+
+if __name__ == '__main__':
+    app.start()
